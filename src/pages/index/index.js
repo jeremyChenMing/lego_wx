9c3b8632608a5a19cc06758086a38197cc70ea78
Jeremy <chenming@upvi.com>
2018-07-31 18:45:09 +0800

    提交小程序

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,69 +1,115 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+let startPoint = null;
+let disX;
+let l;
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    show: true,
+    show: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     tabs: ["选项一", "选项二", "选项三"],
     activeIndex: 1,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+
+
+
+    tab: 1,
+    arr: ['/images/produce.png','/images/produce.png','/images/produce.png'],
+
+    cells: ['','','','',''],
+    dots: false,
+    auto: false,
+    current: 2,
+    pre: '80rpx',
+    next: '80rpx',
+
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../detail/detail'
+    // wx.navigateTo({
+    //   url: '../detail/detail'
+    // })
+  },
+  setNavTitle: function (id) { //设置navbar的文字
+    const ids = this.data.tab
+    wx.setNavigationBarTitle({
+      title: ids == 1 ? '推荐作品' : '所有作品'
+    })
+  },
+
+  handleNav: function(e) {
+    console.log(e)
+    const dataSet = e.currentTarget.dataset
+    this.setData({
+      tab: dataSet.id
     })
   },
   onLoad: function () {
-    const that = this
+    const that = this;
+    const n = 0;
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-
-      
     }
+
+
     setTimeout(function() {
       // that.setData({show: true})
     },1500)
-   
-    
+
 
   },
   
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+
+  handleChange: function (event) {
+    console.log(event)
+    if (event.detail.source === "touch") {
+      // 用户操作
+      this.setData({
+        current: event.detail.current
+      })
+    }
+  },
+
+
+
+  onShareAppMessage: function (res) {
+    console.log(res)
+    if (res.from === 'menu') {
+      // 来自右上角的转发菜单
+    }else if (res.from === 'button') {
+      // 来自按钮转发
+    }
+    return {
+      title: '小霸王其乐无穷',
+      path: '/pages/index?id=123',
+      imageUrl: '/images/produce.png'
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
 })
--- src/pages/logs/logs.js
+++ src/pages/logs/logs.js
@@ -1,16 +1,41 @@
 //logs.js
 const util = require('../../utils/util.js')
+const app = getApp()
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    userInfo: {},
+    tab: 1,
+    arr: ['/images/produce.png','/images/produce.png','/images/produce.png']
   },
   onLoad: function () {
-    this.setData({
-      // logs: (wx.getStorageSync('logs') || []).map(log => {
-      //   return util.formatTime(new Date(log))
-      // })
-      logs: []
+    if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo)
+      this.setData({
+        userInfo: app.globalData.userInfo,
+      })
+    }
+    // this.getList()
+  },
+  getList: function () {
+    wx.request({
+      url: 'https://api.d.51bricks.com',
+      method: 'GET',
+      dataType: 'JSON',
+      success: function (data) {
+        console.log(data)
+      }
     })
-  }
+  },
+  handleNav: function(e) {
+    console.log(e)
+    const dataSet = e.currentTarget.dataset
+    this.setData({
+      tab: dataSet.id
+    })
+  },
+
+
+
 })
